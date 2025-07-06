54d8d53d1f52cc09bcae4535c729e27d69371e80
Ommpiri <ommpiri21@gmail.com>
2025-07-06 09:38:29 +0530

    change

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 int main(){
     int a,b;
     printf("Enter two no\n");
+    printf("Enter two no\n");
     scanf("%d%d",&a,&b);
     a>=b ? printf("%d is greater",a):printf("%d is greater",b);
     return 0;
